00479fca96dd76a767631211f07b0b55ac76f584
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-04 22:18:39 +0000

    Refactored and added test files.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,23 @@
 const request = require('request');
-const breed = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('Breed not found.');
-  }
-  if (data[0]) {
-    console.log('Description: ', data[0].description);
-  }
-});
+
+const fetchBreedDescription = function(breed, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(null, 'Breed not found.');
+      return;
+    } else { 
+      (data[0]) 
+      callback(error, data[0].description);
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
